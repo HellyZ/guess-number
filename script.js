3209fccce231b81b136572e0bff1b256ac771ac3
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-08 11:07:08 +0300

    Revert "refactor recursion"
    This reverts commit 41357776c18455ff1d28fca75b378df7cc3f9053.

--- script.js
+++ script.js
@@ -10,30 +10,48 @@ const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num) && !isNull(num);
 };
 
-function guessNumber(number, attempts) {
-  let attemptsLeft = attempts ? attempts : 10;
-  console.log(number);
-  let guess = prompt("Угадай число от 1 до 100");
-  isNumber(guess) ? null : guessNumber(number, attemptsLeft);
-  if (guess > number) {
-    attemptsLeft--;
-    alert(`Загаданное число меньше, осталось попыток ${attemptsLeft}`);
-    guessNumber(number, attemptsLeft);
-  } else if (guess < number) {
-    alert(`Загаданное число больше, осталось попыток ${attemptsLeft}`);
-    attemptsLeft--;
-    guessNumber(number, attemptsLeft);
-  } else {
+function guessNumber() {
+  const num = Math.floor(Math.random() * (100 - 1) + 1);
+  console.log(num);
+  let count = 10;
 
-    let check = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще? ");
-    if (check) {
-      return;
+  function ask(count) {
+    console.log("ask function");
+    if (count > 0) {
+      let input = prompt("Угадай число от 1 до 100");
+      console.log(count);
+      if (isNumber(input)) {
+        if (+input > num) {
+          count--;
+          alert(`Загаданное число меньше, осталось попыток ${count}`);
+          ask(count);
+        } else if (+input < num) {
+          alert(`Загаданное число больше, осталось попыток ${count}`);
+          count--;
+          ask(count);
+        } else {
+          if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+            count = 10;
+            ask(count);
+          }
+        }
+      } else {
+        if (isNull(input)) {
+          alert("Игра окончена!");
+        } else {
+          alert("Введите число!");
+          ask(count);
+        }
+      }
     } else {
-      console.log("Конец игры");
-      return;
+      if (confirm("Попытки закончились, хотите сыграть еще?")) {
+        count = 10;
+        ask(count);
+      }
     }
   }
+
+  ask(count);
 }
 
-guessNumber(Math.floor(Math.random() * (100 - 1) + 1));
-
+guessNumber();
